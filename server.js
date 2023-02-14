7f991b4b3fb8eebed76fecb00c2d98a167811c13
shohan-pherones <shohan.sub.56@gmail.com>
2023-02-14 15:34:34 +0600

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const projectRoutes = require("./routes/projectRoute");
 
@@ -12,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
