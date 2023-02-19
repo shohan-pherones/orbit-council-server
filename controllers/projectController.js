a5354e44a87c11af73005223f9e95b5e698cc128
shohan-pherones <shohan.sub.56@gmail.com>
2023-02-20 03:58:39 +0600

    user assigned

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -3,7 +3,9 @@ const Project = require("../models/projectModel");
 
 // get all projects
 const getProjects = async (req, res) => {
-  const projects = await Project.find({}).sort({ createdAt: -1 }); // newest one at the top --> descending
+  const user_id = req.user._id;
+
+  const projects = await Project.find({ user_id }).sort({ createdAt: -1 }); // newest one at the top --> descending
 
   res.status(200).json(projects);
 };
@@ -62,8 +64,10 @@ const createProject = async (req, res) => {
   }
 
   try {
+    const user_id = req.user._id;
     const project = await Project.create({
       ...req.body,
+      user_id,
     });
 
     res.status(200).json(project);
--- middlewares/requireAuth.js
+++ middlewares/requireAuth.js
@@ -17,7 +17,6 @@ const requireAuth = async (req, res, next) => {
     req.user = await User.findOne({ _id }).select("_id");
     next();
   } catch (err) {
-    console.log(err);
     res.status(401).json({ error: "Request is not authorized" });
   }
 };
